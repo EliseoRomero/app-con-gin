da7019c39df94b44a0b9b77a12239ff2201d4530
EliseoRomero <eliseo.romero@mercadolibre.com>
2021-12-22 14:59:13 -0300

    Go gin

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//Ejempro de Go Web Gin
+/*
+func main() {
+	r := gin.Default()
+	r.GET("/home", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"Buenas": "Goland",
+		})
+	})
+	r.Run()
+}
+*/
 
 func main() {
-	fmt.Println("Hola Goland")
+	router := gin.Default()
+
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Bienvenido a nuestra primera app de Go con Gin %s", name)
+	})
+	router.Run(":8080")
 }
